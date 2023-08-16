1f26a4f71e900e3f0b83f8e756d871e8da6333c8
kye888888884 <k37237280@gmail.com>
2023-08-16 23:02:02 +0900

    [UPDATE] Improve search of colors

--- app/main/data.py
+++ app/main/data.py
@@ -12,21 +12,11 @@ status_dict = {
 class AnimalData:
     def __init__(self, data_path):
         self.data_path = data_path
-        self.data_species = None
-        self.data_haircolor = None
         self.data = None
         self.load_data()
     
     def load_data(self):
-        self.data = pd.read_csv(self.data_path + "유기동물_데이터.csv")
-
-        self.data_species = pd.read_csv(self.data_path + "unique/species.csv")
-        self.data_species = self.data_species.dropna()
-        self.data_species = self.data_species.sort_values(by=["species"])
-
-        self.data_haircolor = pd.read_csv(self.data_path + "unique/hairColor.csv")
-        self.data_haircolor = self.data_haircolor.dropna()
-        self.data_haircolor = self.data_haircolor.sort_values(by=["hairColor"])
+        self.data = pd.read_csv(self.data_path + "animals_data.csv")
 
     def search(self, params):
         print(params)
@@ -37,18 +27,20 @@ class AnimalData:
             expr += f" and gu == {params['gu']}"
         if params["species"] != "":
             expr += f" and species == '{params['species']}'"
-        if params["haircolor"] != "":
-            expr += f" and hairColor == '{params['haircolor']}'"
+        # if params["haircolor"] != "":
+        #     expr += f" and hairColor == '{params['haircolor']}'"
         if params["status"] != "0":
             expr += f" and adoptionStatusCd in {status_dict[params['status']]}"
-            
         
         df_q = self.data.query(expr)
+        # rescueDate를 기준으로 내림차순 정렬
+        df_q = df_q.sort_values(by=["rescueDate"], ascending=False)
         total_count = len(df_q)
-
+        
         p = (int(params["page"]) - 1) * PAGE_COUNT
         df_q = df_q.iloc[p:p + PAGE_COUNT]
         count = len(df_q)
         
         result = df_q.to_json(orient="records")
         return result, count, total_count
+
